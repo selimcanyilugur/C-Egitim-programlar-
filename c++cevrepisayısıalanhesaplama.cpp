0b57a2fe7e9e752cfee79e71ff2a7645beb66ecc
Selimcan Yıluğur <94720684+selimcanyilugur@users.noreply.github.com>
2021-11-20 16:24:56 +0300

    Add files via upload

---
+++ c++cevrepisayısıalanhesaplama.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	float r,alan,cevre;
+	cout<<"yarýçapý girin:";
+	cin>>r;
+	alan=3.14*r*r;
+	cevre=2*(3.14*r);
+	cout<<"alaný :"<<alan<<endl;
+	cout<<"cevresi :"<<cevre;
+	}
