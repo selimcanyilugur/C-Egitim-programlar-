ce170284055d1f6ef39783d8c293c9190de074c1
Selimcan Yıluğur <94720684+selimcanyilugur@users.noreply.github.com>
2021-11-20 16:28:43 +0300

    Create c++ cift sayı mı tek sayı mı programı.cpp

---
+++ c++ cift sayı mı tek sayı mı programı.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int tamsayi;
+	cout<<"birtamsayi girin:";
+	cin>>tamsayi;
+	if(tamsayi%2==0)
+	{
+		cout<<"girdiniz"<<tamsayi<<"sayisi"<<"cift sayidir."<<endl;
+	}
+}
